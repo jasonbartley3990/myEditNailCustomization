2e054aa0d2877d986c584a74a2c0012e4efc313c
Jason Bartley <83716676+jasonbartley3990@users.noreply.github.com>
2023-10-18 13:42:21 -0500

    Initial Commit

---
+++ myEditNailCustomization/myEditNailCustomizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  myEditNailCustomizationApp.swift
+//  myEditNailCustomization
+//
+//  Created by Jason bartley on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct myEditNailCustomizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
